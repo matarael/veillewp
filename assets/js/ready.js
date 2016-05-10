30515cc034f847c2e6ef368030a8335b8ab57145
julien boucry <julien boucry>
2016-05-10 21:46:04 +0200

    link de infinite scroll init

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -2,5 +2,6 @@ jQuery(document).ready(function(){
 /* -------------------------------------- LOADING OBJECTS---------------------------------- */
 	masonry = new Masonry();
     menu = new Menu();
+    //infinitescrollinit = new InfiniteScrollInit();
 /* -------------------------------------- INITIALIZE -------------------------------------- */
 });
